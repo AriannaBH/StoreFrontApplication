401a37885730079a6c3eb1e70900a238f57ecf91
AriannaBH <abh223d@gmail.com>
2020-11-05 03:24:34 -0600

    Add project files.

---
+++ StoreFrontLAB/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Mvc;
+
+namespace StoreFrontLAB.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //[Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Want to know more about us? Read below!";
+
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            return View();
+        }
+
+        public ActionResult Leggings()
+        {
+            return View();
+        }
+
+        public ActionResult Joggers()
+        {
+            return View();
+        }
+
+        public ActionResult Hoodies()
+        {
+            return View();
+        }
+
+        public ActionResult SportsBras()
+        {
+            return View();
+        }
+
+        public ActionResult ShoppingCart()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ StoreFrontLAB/Controllers/TransactionsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StoreFront.DATA.EF;
+
+namespace StoreFrontLAB.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class TransactionsController : Controller
+    {
+        private storeFrontEntities db = new storeFrontEntities();
+
+        // GET: Transactions
+        public ActionResult Index()
+        {
+            var transactions = db.Transactions.Include(t => t.Customer).Include(t => t.PaymentMethod).Include(t => t.Product);
+            return View(transactions.ToList());
+        }
+
+        // GET: Transactions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // GET: Transactions/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name");
+            ViewBag.PaymentMethodID = new SelectList(db.PaymentMethods, "PaymentMethodID", "Type");
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "Name");
+            return View();
+        }
+
+        // POST: Transactions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrderID,CustomerID,Price,TransactionDate,PaymentMethodID,ProductID,Quantity")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", transaction.CustomerID);
+            ViewBag.PaymentMethodID = new SelectList(db.PaymentMethods, "PaymentMethodID", "Type", transaction.PaymentMethodID);
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "Name", transaction.ProductID);
+            return View(transaction);
+        }
+
+        // GET: Transactions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", transaction.CustomerID);
+            ViewBag.PaymentMethodID = new SelectList(db.PaymentMethods, "PaymentMethodID", "Type", transaction.PaymentMethodID);
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "Name", transaction.ProductID);
+            return View(transaction);
+        }
+
+        // POST: Transactions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrderID,CustomerID,Price,TransactionDate,PaymentMethodID,ProductID,Quantity")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transaction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", transaction.CustomerID);
+            ViewBag.PaymentMethodID = new SelectList(db.PaymentMethods, "PaymentMethodID", "Type", transaction.PaymentMethodID);
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "Name", transaction.ProductID);
+            return View(transaction);
+        }
+
+        // GET: Transactions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // POST: Transactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Transaction transaction = db.Transactions.Find(id);
+            db.Transactions.Remove(transaction);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
